96da43c6b6275ee2fa6878335394553f65c3e5ce
Sergei Pertsev <sergei_pertsev@epam.com>
2021-08-01 22:50:38 +0300

    Refactored a bit and added tests

---
+++ spark-core-homework/task-one/src/test/java/SqlHandlerOfCsvTaskOneTest.java
@@ -0,0 +1,28 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SqlHandlerOfCsvTaskOneTest {
+
+    @Test
+    public void processCsvWithFirstQuery() {
+    }
+
+    @Test
+    public void prepareSqlQueryTextTestShouldReturnCorrectSqlQueryText() {
+        String correctSqlQueryText =
+                        "SELECT hotel_continent,\n" +
+                        "       hotel_country,\n" +
+                        "       hotel_market,\n" +
+                        "       COUNT(*) AS number_of_rows\n" +
+                        "FROM train\n" +
+                        "WHERE srch_adults_cnt = 2\n" +
+                        "GROUP BY hotel_continent,\n" +
+                        "         hotel_country,\n" +
+                        "         hotel_market\n" +
+                        "ORDER BY number_of_rows DESC\n" +
+                        "LIMIT 3";
+        Assert.assertEquals(correctSqlQueryText, SqlHandlerOfCsvTaskOne.prepareSqlQueryText());
+    }
+}
